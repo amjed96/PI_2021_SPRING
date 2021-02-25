a61f5e2c9e24c551d6876b8c5398220ead2adc70
amjed96 <mohamedamjed.bouallegui@esprit.tn>
2021-02-25 10:13:32 +0100

    first commit

---
+++ src/main/java/com/pi/demo/Pi2021Application.java
@@ -0,0 +1,13 @@
+package com.pi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi2021Application.class, args);
+	}
+
+}
